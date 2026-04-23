2588c0d04ff23681dc4520f5722e236d59d00a9f
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2026-04-23 08:32:50 +0200

    Cleanup, Remove old src & tests

--- src-old/core/helpers/interaction.ts
+++
@@ -1,7 +0,0 @@
-import { InteractionStateSnapshot } from '../state/interactionTypes';
-
-export function isInteractionTargetingActive(
-	snapshot: Pick<InteractionStateSnapshot, 'dragSession' | 'releaseTargetingActive'>
-): boolean {
-	return snapshot.dragSession !== null || snapshot.releaseTargetingActive;
-}
--- src-old/core/helpers/util.ts
+++
@@ -1,7 +0,0 @@
-export function setsEqual<T>(a: ReadonlySet<T>, b: ReadonlySet<T>): boolean {
-	if (a.size !== b.size) return false;
-	for (const value of a) {
-		if (!b.has(value)) return false;
-	}
-	return true;
-}
