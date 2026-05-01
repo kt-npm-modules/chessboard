6c108b662bb6806527b7c98828b7de39f6c860f6
Konstantin Tarmyshov <67151862+ktarmyshov@users.noreply.github.com>
2026-05-01 10:11:27 +0200

    Export public input types for wrapper packages. (#50)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 export type { MoveOutput } from './extensions/build/index.js';
-export type { MoveRequestInput, PiecePositionRecordString } from './state/board/types/input.js';
-export type { MoveDestinationInput } from './state/interaction/types/input.js';
+export type {
+	ColorInput,
+	MoveRequestInput,
+	PiecePositionRecordString,
+	PositionInput
+} from './state/board/types/input.js';
+export type { MovabilityInput, MoveDestinationInput } from './state/interaction/types/input.js';
 export { createBoard } from './wrapper/factory.js';
 export type {
 	Chessboard,
